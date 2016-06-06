f2b4102239a64f862bd56272b2c05931eaf29695
Fernando Willian <feh.wilinando@gmail.com>
2016-06-06 14:46:08 -0300

    Criado ViewController para formulario de contato

---
+++ view/FormularioViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FormularioViewController.swift
+//  ContatosApp
+//
+//  Created by Fernando on 06/06/16.
+//  Copyright © 2016 Fernando. All rights reserved.
+//
+
+import UIKit
+
+class FormularioViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
