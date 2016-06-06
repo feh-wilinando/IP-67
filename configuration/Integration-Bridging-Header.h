018c349ed234a537cd59595a593a73769df3b738
Fernando Willian <feh.wilinando@gmail.com>
2016-06-06 14:57:34 -0300

    Criado dao para contato e criado o bridging-header para utilizar a classe modelo no dao

---
+++ configuration/Integration-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Integration-Bridging-Header.h
+//  ContatosApp
+//
+//  Created by Fernando on 06/06/16.
+//  Copyright © 2016 Fernando. All rights reserved.
+//
+
+#ifndef Integration_Bridging_Header_h
+#define Integration_Bridging_Header_h
+
+#import "Contato.h"
+
+#endif /* Integration_Bridging_Header_h */
---
+++ dao/ContatoDAO.swift
@@ -0,0 +1,30 @@
+//
+//  ContatoDAO.swift
+//  ContatosApp
+//
+//  Created by Fernando on 06/06/16.
+//  Copyright © 2016 Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ContatoDAO: NSObject {
+    static internal var defaultDAO: ContatoDAO!;
+    var contatos: Array<Contato> = Array()
+    
+    
+    override private init(){}
+    
+    static func sharedInstance() -> ContatoDAO{
+        if ContatoDAO.defaultDAO == nil {
+            ContatoDAO.defaultDAO = ContatoDAO()
+        }
+        
+        return ContatoDAO.defaultDAO
+    }
+    
+    func add(contato:Contato){
+        self.contatos.append(contato)
+    }
+    
+}
